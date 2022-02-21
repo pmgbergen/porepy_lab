6fa760be733784c39eda49cfbcb0443e1f383b3d
Jakub Both <jakub.both@uib.no>
2022-02-21 21:23:08 +0100

    monolithic version works

--- polimi_course_poroelasticity_solvers/footingproblem.py
+++ polimi_course_poroelasticity_solvers/footingproblem.py
@@ -6,31 +6,10 @@ import scipy.sparse as sps
 # Details of class not relevant - needed to setup exercise
 class FootingProblem(pp.ContactMechanicsBiot):
 
-    def __init__(self, params, lame_mu=1, lame_lambda=1, force=0, mesh_resolution=4, kappa = 1, s0 = 1, alpha = 1, time_step = 1):
+    def __init__(self, params):
         super().__init__(params)
 
-        self.lame_mu = lame_mu
-        self.lame_lambda = lame_lambda
-        self.force = force
-
-        self.kappa = kappa
-        self.mass_weight = s0
-
-        self.alpha = 1
-    
-        self.nx = int(mesh_resolution)
-        self.ny = int(mesh_resolution)
-
-        self.time_step = time_step
-
-    def prepare_simulation(self) -> None:
-        super().prepare_simulation()
-
-        self.viz = pp.Exporter(
-            self.gb,
-            file_name=self.params["file_name"],
-            folder_name=self.params["folder_name"],
-        )
+        self.time_step = self.params.get("time_step", 1.)
 
     def create_grid(self) -> None:
         """Create the grid bucket.
@@ -43,20 +22,23 @@ class FootingProblem(pp.ContactMechanicsBiot):
                 maximum values in each dimension.
         """
         phys_dims = [1, 1]
-        n_cells = [self.nx, self.ny]
+        N = self.params.get("mesh_resolution", 1)
+        n_cells = [N, N]
         self.box: Dict = pp.geometry.bounding_box.from_points(
-            np.array([[0, 0], phys_dims])
+            np.transpose(np.array([[0, 0], phys_dims]))
         )
         g: pp.Grid = pp.CartGrid(n_cells, phys_dims)
         g.compute_geometry()
         self.gb: pp.GridBucket = pp.meshing._assemble_in_bucket([[g]])
 
     def _set_mechanics_parameters(self) -> None:
+        super()._set_mechanics_parameters()
+
         gb = self.gb
         for g, d in gb:
             # Rock parameters
-            lam = np.ones(g.num_cells) / self.scalar_scale
-            mu = np.ones(g.num_cells) / self.scalar_scale
+            lam = self.params.get("lame_lambda", 1.) * np.ones(g.num_cells) / self.scalar_scale
+            mu = self.params.get("lame_mu", 1.) * np.ones(g.num_cells) / self.scalar_scale
             C = pp.FourthOrderTensor(mu, lam)
 
             # Define boundary condition
@@ -81,9 +63,11 @@ class FootingProblem(pp.ContactMechanicsBiot):
             )
 
     def _set_scalar_parameters(self) -> None:
+        super()._set_scalar_parameters()
+
         tensor_scale = self.scalar_scale / self.length_scale ** 2
-        kappa = self.kappa * tensor_scale
-        mass_weight = self.mass_weight * self.scalar_scale
+        kappa = self.params.get("kappa", 1.) * tensor_scale
+        mass_weight = self.params.get("s0", 1.) * self.scalar_scale
         for g, d in self.gb:
             bc = self._bc_type_scalar(g)
             bc_values = self._bc_values_scalar(g)
@@ -94,7 +78,7 @@ class FootingProblem(pp.ContactMechanicsBiot):
                 kappa * specific_volume * np.ones(g.num_cells)
             )
 
-            alpha = self.alpha * self._biot_alpha(g)
+            alpha = self.params.get("alpha", 1.) * self._biot_alpha(g)
             pp.initialize_data(
                 g,
                 d,
@@ -113,29 +97,21 @@ class FootingProblem(pp.ContactMechanicsBiot):
             )
 
     def _bc_type_mechanics(self, g) -> pp.BoundaryConditionVectorial:
-        all_bf = g.get_boundary_faces()
-        bc = pp.BoundaryConditionVectorial(g, all_bf, "neu")
         # Apply fix on the bottom - Dirichlet boundary conditions
-        bottom_face = np.nonzero(g.face_centers[1,all_bf] < self.box["ymin"] + 1e-6)[0] 
-        bc.is_dir[:, bottom_face] = True
-        bc.is_neu[:, bottom_face] = False
-
-        return bc
+        all_bf, *_ = self._domain_boundary_sides(g)
+        bottom_face = np.nonzero(g.face_centers[1,:] < self.box["ymin"] + 1e-6)[0]
+        return pp.BoundaryConditionVectorial(g, bottom_face, "dir")
 
     def _bc_type_scalar(self, g: pp.Grid) -> pp.BoundaryCondition:
-        # Define boundary regions
-        all_bf, *_ = self._domain_boundary_sides(g)
-        # Define boundary condition on faces - Dirichlet per default
-        bc = pp.BoundaryCondition(g, all_bf, "dir")
         # No flow on top and bottom
-        top_face = np.nonzero(g.face_centers[1,all_bf] > self.box["ymax"] - 1e-6)[0] 
-        bottom_face = np.nonzero(g.face_centers[1,all_bf] < self.box["ymin"] + 1e-6)[0] 
-        bc.is_dir[top_face] = False
-        bc.is_neu[top_face] = True
-        bc.is_dir[bottom_face] = False
-        bc.is_neu[bottom_face] = True
-
-        return bc
+        all_bf, *_ = self._domain_boundary_sides(g)
+        dir_face = np.nonzero(
+                np.logical_or(
+                    g.face_centers[0,:] > self.box["xmax"] - 1e-6,
+                    g.face_centers[0,:] < self.box["xmin"] + 1e-6,
+                )
+            )[0]
+        return pp.BoundaryCondition(g, dir_face, "dir")
 
     def _bc_values_mechanics(self, g: pp.Grid) -> np.ndarray:
         """Set homogeneous conditions on all boundary faces."""
@@ -143,8 +119,13 @@ class FootingProblem(pp.ContactMechanicsBiot):
         all_bf, *_ = self._domain_boundary_sides(g)
         values = np.zeros((self._Nd, g.num_faces))
         # Downward force on top boundary
-        top_face = np.nonzero(g.face_centers[1,all_bf] > self.box["ymax"] - 1e-6)[0] 
-        values[1,top_face] = -self.force
+        half_top_face = np.nonzero(
+                np.logical_and(
+                    g.face_centers[1,:] > self.box["ymax"] - 1e-6,
+                    np.absolute(g.face_centers[0,:] - 0.5 * (self.box["xmin"] + self.box["xmax"])) < 0.25,
+                )
+            )[0] 
+        values[1,half_top_face] = self.params.get("force", 0.)
         # Reshape according to PorePy convention
         values = values.ravel("F")
         return values
@@ -184,8 +165,15 @@ class FootingProblem(pp.ContactMechanicsBiot):
         return self._nonlinear_iteration
 
     def export(self, time_step: int) -> None:
-        data = [self.displacement_variable, self.scalar_variable]
-        self.viz.write_vtu(data, time_step = time_step)
+        grid_list = [g for g, _ in self.gb.nodes()]
+        g = grid_list[0]
+        d = self.gb.node_props(g)
+
+        displacement = np.zeros((3, g.num_cells))
+        displacement[:2,:] = np.reshape(d[pp.STATE][self.displacement_variable], (2,g.num_cells), 'F')
+        pressure = d[pp.STATE][self.scalar_variable]
+
+        self.viz.write_vtu({"u": displacement, "p": pressure}, time_step = time_step)
 
 ##########################
 # Main starts here
@@ -193,9 +181,19 @@ class FootingProblem(pp.ContactMechanicsBiot):
 
 # Define model parameters
 model_params = {
+        # DO NOT CHANGE THESE:
         "file_name" : "poroelasticity",   # exporter
         "folder_name" : "out",  # expoter
         "use_ad" : True,
+        # FEEL FREE TO CHANGE ANY OF THOSE:
+        "lame_mu": 1e8,
+        "lame_lambda": 1e7,
+        "force": -1e6,
+        "kappa": 1e-12,
+        "s0": 1e-10,
+        "alpha": 1.,
+        "mesh_resolution": 100,
+        "time_step": 0.25,
         }
 
 # Construct model - FluidFlower benchmark geometry, running a well test
@@ -219,32 +217,32 @@ while model.time < model.end_time:
         # Empty method, but include for completeness
         model.before_newton_iteration()
 
-        # Assemble and solve for the increment
-        if first_call:
-            A, _ = model.assemble_linear_system()
-            A_splu = sps.linalg.splu(A)
-            first_call = False
+        ## Assemble and solve for the increment
+        #if first_call:
+        #    A, _ = model.assemble_linear_system()
+        #    A_splu = sps.linalg.splu(A)
+        #    first_call = False
 
-        _, residual = model.assemble_linear_system()
-        increment = A_splu.solve(residual)
-
-        print(increment.shape)
+        #_, residual = model.assemble_linear_system()
+        #increment = A_splu.solve(residual)
+        increment = model.assemble_and_solve_linear_system(tol=1e-6)
 
         # Error management
         increment_norm = np.linalg.norm(increment)
-        residual_norm = np.linalg.norm(residual)
-        if model.nonlinear_iteration() == 0:
-            residual_norm_ref = residual_norm
-            if residual_norm_ref < 1e-10:
-                residual_norm_ref = 1.
+        #residual_norm = np.linalg.norm(residual)
+        #if model.nonlinear_iteration() == 0:
+        #    residual_norm_ref = residual_norm
+        #    if residual_norm_ref < 1e-10:
+        #        residual_norm_ref = 1.
 
         # Manage the iteration (accumulate to determine the solution)
         model.after_newton_iteration(increment)
 
-        print("time step", model.time_step, "current time", model.time, "it count", model.nonlinear_iteration(), residual_norm, residual_norm / residual_norm_ref)
+        #print("time step", model.time_step, "current time", model.time, "it count", model.nonlinear_iteration(), residual_norm, residual_norm / residual_norm_ref)
         # Check for convergence
-        if residual_norm < 1e-5:
+        if True or residual_norm < 1e-5:
             # Finish off
+            #model.after_newton_convergence(increment, 0, 0)
             model.after_newton_convergence()
             model.export(time_step)
 
